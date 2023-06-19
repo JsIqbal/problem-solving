0e90e2b38e161dec6a56924683eb8acb89b82925
jsKraken <javascriptiqbal@gmail.com>
2023-06-19 20:16:36 +0600

    Add URI 21

--- uri-21.cpp
+++ uri-21.cpp
@@ -20,7 +20,7 @@ int main() {
     int A, B, C, D;
     cin >> A >> B >> C >> D;
     
-    if (C > 0 && D > ) {
+    if (C > 0 && D > 0) {
         if (A%2 == 0) {
             if (B > C && D > A) {
                 if ((C + D) > (A + B)) {
