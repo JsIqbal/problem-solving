5a80847f4ee50120ed72c063377928078dc89936
Iqbal Hossain <110350034+JsIqbal@users.noreply.github.com>
2023-06-30 16:59:14 +0600

    Create evenNumbers.cpp

---
+++ beeCrowd/evenNumbers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    for (int i = 2; i<101; i++) {
+        if (i%2 == 0) {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
